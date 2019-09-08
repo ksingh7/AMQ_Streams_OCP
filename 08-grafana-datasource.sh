2c7a88487ac3a42d74c60005e0d24acb88b9b9e7
ksingh7 <karan.singh731987@gmail.com>
2019-09-08 20:32:55 +0530

    fixes

--- 08-grafana-datasource.sh
+++ 08-grafana-datasource.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # create Prometheus datasource
-curl -X POST http://admin:admin@grafana-route-amq.apps.cluster-str-5110.str-5110.open.redhat.com/api/datasources -d @grafana-dashboards/datasource.json --header "Content-Type: application/json"
+curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/datasources -d @grafana-dashboards/datasource.json --header "Content-Type: application/json"
 # deploy Kafka dashboard
-curl -X POST http://admin:admin@grafana-route-amq.apps.cluster-str-5110.str-5110.open.redhat.com/api/dashboards/db -d @grafana-dashboards/strimzi-kafka.json --header "Content-Type: application/json"
+curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/strimzi-kafka.json --header "Content-Type: application/json"
 # deploy Zookeeper dashboard
-curl -X POST http://admin:admin@grafana-route-amq.apps.cluster-str-5110.str-5110.open.redhat.com/api/dashboards/db -d @grafana-dashboards/strimzi-zookeeper.json --header "Content-Type: application/json"
+curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/strimzi-zookeeper.json --header "Content-Type: application/json"
+
+## Note : Manually importing the Ceph dashboard works. This needs to be fixed
+
+# deploy Ceph dashboard
+#curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/ceph-cluster-dashboard.json --header "Content-Type: application/json"
+# deploy Ceph dashboard
+#curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/ceph-osds-dashboard.json --header "Content-Type: application/json"
+# deploy Ceph dashboard
+#curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/ceph-pools-dashboard.json --header "Content-Type: application/json"
